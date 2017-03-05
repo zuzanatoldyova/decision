642bdb31f8a4bca8546737af08114a8dc7b7c60e
Kirat Virk <kirat_virk3@yahoo.ca>
2017-03-05 22:57:35 +0000

    clean up

--- server/lib/twilio.js
+++ server/lib/twilio.js
@@ -1,25 +1,25 @@
-// const accountSid    = process.env.TWILIO_ACCOUNT_SID;
-// const authToken     = process.env.TWILIO_AUTH_TOKEN;
-// const twilioNumber  = process.env.TWILIO_NUMBER;
-// const myNumber      = process.env.MY_NUMBER;
-// const twilio        = require('twilio');
-// const client        = new twilio.RestClient(accountSid, authToken);
+const accountSid    = process.env.TWILIO_ACCOUNT_SID;
+const authToken     = process.env.TWILIO_AUTH_TOKEN;
+const twilioNumber  = process.env.TWILIO_NUMBER;
+const myNumber      = process.env.MY_NUMBER;
+const twilio        = require('twilio');
+const client        = new twilio.RestClient(accountSid, authToken);
 
-// function sendSmsInvite(user, link, recipient = myNumber) {
-//   client.messages.create({
-//     body: `${user} invites you to vote for a new poll, you can vote on this link: ${link}`,
-//     to: myNumber,
-//     from: twilioNumber
-//   }, function(err, message) {
-//     console.log(err);
-//   });
-// }
+function sendSmsInvite(user, link, recipient = myNumber) {
+  client.messages.create({
+    body: `${user} invites you to vote for a new poll, you can vote on this link: ${link}`,
+    to: myNumber,
+    from: twilioNumber
+  }, function(err, message) {
+    console.log(err);
+  });
+}
 
-// module.exports = {
+module.exports = {
 
-//   sendSmsInvites: (user, link, recipients) => {
-//     recipients.forEach(recipient => {
-//       sendSmsInvite(user, link, recipient);
-//     });
-//   }
-// };
+  sendSmsInvites: (user, link, recipients) => {
+    recipients.forEach(recipient => {
+      sendSmsInvite(user, link, recipient);
+    });
+  }
+};
